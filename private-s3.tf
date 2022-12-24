5fa65d7dad63d4a37569f8a877e187ece5b2d2d4
masarakki <masaki182@gmail.com>
2022-12-25 01:33:11 +0900

    remove-intelligent-tiering-configuration

--- private-s3.tf
+++ private-s3.tf
@@ -11,19 +11,3 @@ resource "aws_s3_bucket_acl" "masarakki-photos-acl" {
   bucket = aws_s3_bucket.masarakki-photos.id
   acl    = "private"
 }
-
-resource "aws_s3_bucket_intelligent_tiering_configuration" "photos-entier-bucket" {
-  bucket   = aws_s3_bucket.masarakki-photos.bucket
-  name     = "EntierBucket"
-  provider = aws.global
-
-  tiering {
-    access_tier = "ARCHIVE_ACCESS"
-    days        = 60
-  }
-
-  tiering {
-    access_tier = "DEEP_ARCHIVE_ACCESS"
-    days        = 180
-  }
-}
