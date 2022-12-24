8736cfe68d56ec1666b189a6b5ca53f94fcec764
masarakki <masaki182@gmail.com>
2022-12-25 00:33:25 +0900

    fix-bucket-acl

--- private-s3.tf
+++ private-s3.tf
@@ -1,11 +1,15 @@
 resource "aws_s3_bucket" "masarakki-photos" {
   bucket   = "masarakki-photos"
-  acl      = "private"
   provider = aws.global
 
   tags = {
     Project = "private"
   }
+}
+
+resource "aws_s3_bucket_acl" "masarakki-photos-acl" {
+  bucket = aws_s3_bucket.masarakki-photos.id
+  acl    = "private"
 }
 
 resource "aws_s3_bucket_intelligent_tiering_configuration" "photos-entier-bucket" {
