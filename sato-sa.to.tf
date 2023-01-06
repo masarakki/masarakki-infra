118d43cf66685272b48af90ba87aa21539d81f4c
masarakki <masaki182@gmail.com>
2023-01-07 01:13:52 +0900

    fix-cert-region

--- sato-sa.to.tf
+++ sato-sa.to.tf
@@ -7,6 +7,7 @@ resource "aws_route53_zone" "sato-sato" {
 
 resource "aws_acm_certificate" "sato-sato-cert" {
   domain_name       = aws_route53_zone.sato-sato.name
+  provider          = aws.global
   validation_method = "DNS"
   subject_alternative_names = [
     "*.${aws_route53_zone.sato-sato.name}"
