ad108b87ad10a0414cb875e567fdceb5a42fc2c9
masarakki <masaki182@gmail.com>
2023-01-07 01:08:53 +0900

    create-np-complete-cert

--- np-complete.tf
+++ np-complete.tf
@@ -4,3 +4,15 @@ resource "aws_route53_zone" "np-complete" {
     Project = "np-complete"
   }
 }
+
+resource "aws_acm_certificate" "np-complete-cert" {
+  provider          = aws.global
+  domain_name       = aws_route53_zone.np-complete.name
+  validation_method = "DNS"
+  subject_alternative_names = [
+    "*.${aws_route53_zone.np-complete.name}"
+  ]
+  tags = {
+    Project = "np-complete"
+  }
+}
